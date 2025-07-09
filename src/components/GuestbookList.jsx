335af4254535c892e4bdc818a2fd243e4ebae38e
leeeeegun <leeeeegun@naver.com>
2025-07-09 14:52:33 +0900

    feat: [이의건] GuestbookList - 글 목록 컴포넌트 추가 [AGB-4]

---
+++ src/components/GuestbookList.jsx
@@ -0,0 +1,17 @@
+
+
+function GuestbookList({ list }) {
+    return (
+        <ul>
+            {list.map((item) => (
+                <li key = {item.id}>
+                    <strong>{item.nickname}</strong>
+                    ({new Date(item.createdAt).toLocaleString()})
+                    <p>{item.content}</p>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default GuestbookList;
