10e350caa09b6a6fe1462ff67f4e1866c4bdbe23
leeeeegun <leeeeegun@naver.com>
2025-07-09 15:50:22 +0900

    fix: [이의건] AxiosError, 500 Error 해결 및 테스트 완료 [AGB-5]

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
+// axios 인스턴스 생성 (공통 baseURL 설정)
 const api = axios.create({
-    baseURL: 'http://localhost:8888/api/guestbook',
+    baseURL: '/api/guestbook',  // 상대경로로 변경
 });
 
-export const postGuestbook = (data) => api.post('/', data);
-export const getGuestbook = () => api.get('/');
+// 방명록 목록 가져오기
+export const getGuestbookList = () => api.get('');  // 빈 문자열로 호출
+export const postGuestbook = (data) => api.post('', data);
 
 export default api;
